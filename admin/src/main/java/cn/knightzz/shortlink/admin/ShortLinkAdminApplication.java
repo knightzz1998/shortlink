caceae0a9360fb43ffc531269c52b5b6045af59b
wangtianci <wangtianci2068@gmail.com>
2023-12-26 15:53:29 +0800

    feature: 项目初始化完成

---
+++ admin/src/main/java/cn/knightzz/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,18 @@
+package cn.knightzz.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王天赐
+ * @title AdminApplication
+ * @description
+ * @create 2023-12-26 15:46
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
